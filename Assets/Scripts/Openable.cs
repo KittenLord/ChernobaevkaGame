a14508d01d831238287997b9868df74ff683b480
KittenLord <vanya.braslavets@gmail.com>
2022-05-05 15:43:41 +0300

    Added functionality for interacting with objects

---
+++ Assets/Scripts/Openable.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Openable : MonoBehaviour, IInteractable
+{
+    public GameObject WindowGameObject;
+    public void Interact()
+    {
+        WindowGameObject.SetActive(true);
+
+        var player = FindObjectOfType<PlayerController>();
+
+        player.CanInteract = false;
+        player.DeactivateMovement = true;
+        player.DeactivateRotation = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,10 +6,14 @@ public class PlayerController : MonoBehaviour
 {
     public float Speed;
     public float SmoothTime;
-    private Vector3 velocity;
     public int PlaneLayer;
 
+    public bool DeactivateMovement = false;
+    public bool DeactivateRotation = false;
 
+    public bool CanInteract = true;
+
+    private Vector3 velocity;
 
     void Start()
     {
@@ -21,29 +25,46 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Movement Detection
-        var direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        if (!DeactivateMovement)
+        {
+            var direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
 
-        var nextPosition = Vector3.SmoothDamp(transform.position, transform.position + direction * Speed, ref velocity, SmoothTime);
+            var nextPosition = Vector3.SmoothDamp(transform.position, transform.position + direction * Speed, ref velocity, SmoothTime);
 
-        transform.position = nextPosition;
+            transform.position = nextPosition;
+        }
 
 
 
         // Rotation Detection
-        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hitinfo;
-        Physics.Raycast(ray, out hitinfo, 9999, 1 << PlaneLayer); // 1 << PlaneLayer makes it only react to the object with layer PlaneLayer
-        var hitPoint = new Vector3(hitinfo.point.x, transform.position.y, hitinfo.point.z); // Make the target point the same height, as the player, so no falling down will occur
+        if (!DeactivateRotation)
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitinfo;
+            Physics.Raycast(ray, out hitinfo, 9999, 1 << PlaneLayer); // 1 << PlaneLayer makes it only react to the object with layer PlaneLayer
+            var hitPoint = new Vector3(hitinfo.point.x, transform.position.y, hitinfo.point.z); // Make the target point the same height, as the player, so no falling down will occur
 
-        transform.LookAt(hitPoint);
+            transform.LookAt(hitPoint);
+        }
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if(other.tag == "Finish" && Input.GetKey(KeyCode.F))
+        if(other.tag == "Interactable")
         {
-            Debug.Log("cocaine");
-            // Interact
+            // TODO: Make a "Press [F] to interact" label appear
+
+
+            //
+
+
+            // When player presses F and interaction is not locked
+
+            if (Input.GetKey(KeyCode.F) && CanInteract)
+            {
+                var interactable = other.GetComponent<IInteractable>();
+                interactable.Interact();
+            }
         }
     }
 }
